c4d8a3a50df03e5e952d2244840827bd85d18da1
psajaev1 <psajaev@gmail.com>
2024-01-23 15:35:11 -0600

    Delete rectangles/target directory

--- rectangles/target/debug/deps/rectangles-d7aeda53491c10f4.d
+++
@@ -1,5 +0,0 @@
-/Users/psajaev/CodeProjects/rustStuff/rectangles/target/debug/deps/librectangles-d7aeda53491c10f4.rmeta: src/main.rs
-
-/Users/psajaev/CodeProjects/rustStuff/rectangles/target/debug/deps/rectangles-d7aeda53491c10f4.d: src/main.rs
-
-src/main.rs:
--- rectangles/target/debug/rectangles.d
+++
@@ -1,1 +0,0 @@
-/Users/psajaev/CodeProjects/rustStuff/rectangles/target/debug/rectangles: /Users/psajaev/CodeProjects/rustStuff/rectangles/src/main.rs
